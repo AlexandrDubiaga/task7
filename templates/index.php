8af63ae1ca790bf7b7686d439378ca2fd268a860
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-09-08 12:56:24 +0300

    Update index.php

--- templates/index.php
+++ templates/index.php
@@ -6,7 +6,7 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
     </head>
     <body>
-        <form>
+        <form method="post">
             <div class="form-group">
                 <input type="text" class="form-control" value="%NAME%" name="name" placeholder="name">
                 <div>%ERROR_NAME%</div>
